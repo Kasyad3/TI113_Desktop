c635ea36960f3505803131a4dbb56a4adf94a0cc
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2025-02-18 20:29:41 -0300

    feat: Exercício do 1 ao 6 concluído.

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem = "";
+
+            Console.Write("Informe o Primeiro Número:");
+            float num1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+            Console.Write("Informe o Segundo Número:");
+            float num2 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+            Console.Write("Informe o Terceiro Número:");
+            float num3 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n"); 
+            Console.WriteLine("\n"); 
+            Console.WriteLine("\n");
+
+            // Encontrar o maior número entre os três usando Math.Max
+            float maior = Math.Max(num1, Math.Max(num2, num3));
+
+            // Encontrar o menor número entre os três usando Math.Min
+            float menor = Math.Min(num1, Math.Min(num2, num3));
+
+
+            // Verifica se todos os números são iguais
+            if (num1 == num2 && num2 == num3)
+            {
+                mensagem = $"Os números {num1}, {num2} e {num3} são iguais.";
+            }
+            else
+            {
+                mensagem = $"O Maior Número é {maior} e o Menor Número é {menor}.";
+            }
+
+            Console.WriteLine(mensagem);
+            
+        }
+    }
+}
---
+++ Exercicio6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem = "";
+
+            Console.Write("Informe seu nome:");
+            string nome = Console.ReadLine()!;
+            Console.WriteLine("\n");
+
+            Console.Write("Informe quanto ganha por hora:");
+            float salario = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+                
+            Console.Write("Informe quantas horas trabalha por mês:");
+            float cargaHoraria = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+
+            float salarioTotal = salario * cargaHoraria;
+
+            mensagem = $"O {nome} recebe no total por horas trabalhadas {salarioTotal}.";
+            Console.WriteLine(mensagem);
+        }
+    }
+}
